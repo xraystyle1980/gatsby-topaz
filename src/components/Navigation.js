6c3646979d9a5c4cb2c28f107ce8f5155440b99e
Matt Trice <matt.trice@gmail.com>
2018-10-14 00:40:54 -0400

    Add responsive menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,48 @@
 import React from "react";
 
+
 const Navigation = () => (
-  <nav className="navbar">
-    <span class="navbar-toggle" id="js-navbar-toggle">
-      <i class="fas fa-bars"></i>
-    </span>
-    <a href="#" className="logo">logo</a>
-    <ul className="main-nav" id="js-menu">
-      <li>
-        <a href="#" className="nav-links">Home</a>
-      </li>
-      <li>
-        <a href="#" className="nav-links">Products</a>
-      </li>
-      <li>
-        <a href="#" className="nav-links">About Us</a>
-      </li>
-      <li>
-        <a href="#" className="nav-links">Contact Us</a>
-      </li>
-      <li>
-        <a href="#" className="nav-links">Blog</a>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <a href="#main-menu"
+       class="menu-toggle"
+       role="button"
+       id="main-menu-toggle"
+       aria-expanded="false"
+       aria-controls="main-menu"
+       aria-label="Open main menu">
+      <span className="sr-only">Open main menu</span>
+      <span className="fa fa-bars" aria-hidden="true"></span>
+    </a>
+
+    <h1 className="logo">hamburgers</h1>
+
+    <nav id="main-menu"
+         className="main-menu"
+         role="navigation"
+         aria-expanded="false"
+         aria-label="Main menu">
+      <a href="#main-menu-toggle"
+         class="menu-close"
+         role="button"
+         id="main-menu-close"
+         aria-expanded="false"
+         aria-controls="main-menu"
+         aria-label="Close main menu">
+        <span className="sr-only">Close main menu</span>
+        <span className="fa fa-close" aria-hidden="true"></span>
+      </a>
+      <ul>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <a href="#main-menu-toggle"
+       className="backdrop"
+       tabindex="-1"
+       aria-hidden="true"
+       hidden></a>
+  </header>
 );
 
 export default Navigation;
