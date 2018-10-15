b810c422ef358956eb8c534f5d71a9641ad5ffc7
Matt Trice <matt.trice@gmail.com>
2018-10-14 23:53:40 -0400

    New simple nav in place

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,40 +3,21 @@ import pic01 from '../assets/images/logo-topaz.svg' ;
 
 
 const Navigation = () => (
-  <header>
-    <a href="#main-menu" class="menu-toggle" role="button" id="main-menu-toggle" aria-expanded="false" aria-controls="main-menu" aria-label="Open main menu">
-      <span className="sr-only">Open main menu</span>
-      <span className="fa fa-bars" aria-hidden="true"></span>
-    </a>
-
-    <a href="#home" className="logo"><img src={pic01} alt="Topaz" /></a>
-
-    <nav id="main-menu"
-         className="main-menu"
-         role="navigation"
-         aria-expanded="false"
-         aria-label="Main menu">
-      <a href="#main-menu-toggle"
-         class="menu-close"
-         role="button"
-         id="main-menu-close"
-         aria-expanded="false"
-         aria-controls="main-menu"
-         aria-label="Close main menu">
-        <span className="sr-only">Close main menu</span>
-        <span className="fa fa-close" aria-hidden="true"></span>
-      </a>
-      <ul>
-        <li><a className="navlinks__desktop--white" href="#">Learn</a></li>
-        <li><a className="navlinks__desktop--white" href="#">About</a></li>
-        <li><a className="navlinks__desktop--white" href="#">Contact</a></li>
-      </ul>
-    </nav>
-    <a href="#main-menu-toggle"
-       className="backdrop"
-       tabindex="-1"
-       aria-hidden="true"
-       hidden></a>
+  <header className="topaz-header">
+    <div className="grid-wrapper">
+      <div className="col-6">
+        <a href="#home" className="logo"><img src={pic01} alt="Topaz" /></a>
+      </div>
+      <div className="col-6">
+        <nav className="navigation__desktop--white" role="navigation" aria-expanded="false" aria-label="Main menu">
+          <ul>
+            <li><a className="navlinks__desktop--white" href="#">Learn</a></li>
+            <li><a className="navlinks__desktop--white" href="#">About</a></li>
+            <li><a className="navlinks__desktop--white" href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   </header>
 );
 
