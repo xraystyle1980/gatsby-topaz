4d97bf6325f040a9d5b594e9da79d2e2f0d2d624
Matt Trice <matt.trice@gmail.com>
2018-10-19 20:23:32 -0400

    Continue loading content, install Shevy for vertical rhythm

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,6 @@ import Mc from '../components/Mc';
 
 import Layout from '../components/layout';
 
-import IconPlaceholderLogo from '../assets/images/icon-placeholder-logo.svg'
-
 class Homepage extends React.Component {
     render() {
         const siteTitle = "Topaz - Blockchain Simplified";
@@ -14,15 +12,46 @@ class Homepage extends React.Component {
             <Layout>
                 <Helmet title={siteTitle} />
 
-                <section id="three" className="features__hero--home">
-
-                </section>
-
-                <section id="four" className="main special">
+                  <section  className="features__hero--home">
+<div className="container">
 
 
+                    <div className="grid-wrapper">
+                      <div className="col-12">
+                        <h2 className="features__headline--dark"><span>Add the value</span> of complex blockchain applications seamlessly to your existing technology stack.</h2>
+                      </div>
+                      <div className="col-6">
+                        <div className="features__colorblock--amber">
+                          <p className="features__paragraph--dark">Topaz arranges your data and performs Ethereum transactions on a set interval to store the data on a blockchain at no cost to you.</p>
+                        </div>
+                      </div>
+                      <div className="col-6">
+                        <h4 className="features__headline--dark">You can now query and furnish your data, and provide proof of existence and chain of custody over time.</h4>
+                      </div>
+                      <div className="col-4">
+                        <div className="features__block--shadow">
+                          <h4>EFFICIENT</h4>
+                          <p>Store data on Ethereum in the most efficient way possible.</p>
+                        </div>
+                      </div>
+                      <div className="col-4">
+                        <div className="features__block--shadow">
+                          <h4>INTEGRATE</h4>
+                          <p>Integrate with any existing codebase via first class packages.</p>
+                        </div>
+                      </div>
+                      <div className="col-4">
+                        <div className="features__block--shadow">
+                          <h4>REPORTING</h4>
+                          <p>Generate reports on the data you store on blockchain.</p>
+                        </div>
+                      </div>
+                    </div>
 
-                </section>
+
+  </div>
+                  </section>
+
 
             </Layout>
         );
