f7d52063c338b8a6291d52aaaaa249682a485263
Matt Trice <matt.trice@gmail.com>
2018-10-21 17:07:52 -0400

    Begin dialing in tablet

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,21 +45,21 @@ class Homepage extends React.Component {
                           <div className="features__block--shadow">
                             <img src={iconEfficient} alt="Efficient" className="features__image--icon" />
                             <h4 className="header--blue">EFFICIENT</h4>
-                            <p>Store data on Ethereum in the most efficient way possible.</p>
+                            <p className="features__paragraph--desc-dark">Store data on Ethereum in the most efficient way possible.</p>
                           </div>
                         </div>
                         <div className="col-4">
                           <div className="features__block--shadow">
                             <img src={iconIntegrate} alt="Integrate" className="features__image--icon" />
                             <h4 className="header--blue">INTEGRATE</h4>
-                            <p>Integrate with any existing codebase via first class packages.</p>
+                            <p className="features__paragraph--desc-dark">Integrate with any existing codebase via first class packages.</p>
                           </div>
                         </div>
                         <div className="col-4">
                           <div className="features__block--shadow">
                             <img src={iconReporting} alt="Reporting" className="features__image--icon" />
                             <h4 className="header--blue">REPORTING</h4>
-                            <p>Generate reports on the data you store on blockchain.</p>
+                            <p className="features__paragraph--desc-dark">Generate reports on the data you store on blockchain.</p>
                           </div>
                         </div>
                       </div>
@@ -69,7 +69,7 @@ class Homepage extends React.Component {
                       <div className="grid-wrapper">
                         <div className="col-12">
                           <div className="features__colorblock">
-                            <p className="features__paragraph--light"><span>Topaz arranges your data</span> and performs Ethereum transactions on a set interval to store the data on a blockchain at no cost to you.</p>
+                            <p className="features__paragraph--light-heavy"><span>Topaz arranges your data</span> and performs Ethereum transactions on a set interval to store the data on a blockchain at no cost to you.</p>
                             <img src={FeaturesMultiGraphic} alt="Topaz" className="features__image--multigraphic" />
                           </div>
                         </div>
@@ -80,7 +80,7 @@ class Homepage extends React.Component {
                       <div className="grid-wrapper">
                         <div className="col-12">
                           <div className="features__block--under">
-                            <p className="features__paragraph--dark-larger">You can now query and furnish your data, and provide <span>proof of existence and chain of custody</span> over time.</p>
+                            <p className="features__paragraph--dark-heavy">You can now query and furnish your data, and provide <span>proof of existence and chain of custody</span> over time.</p>
                           </div>
                         </div>
                       </div>
