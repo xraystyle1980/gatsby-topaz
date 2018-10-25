b1ba495b14a5f6c9f3ae0a1646416a37f55796bf
Matt Trice <matt.trice@gmail.com>
2018-10-24 20:45:11 -0400

    spell check

--- config/website.js
+++ config/website.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Topaz | Proof of existince in one line of code', // Navigation and Site Title
+  siteTitle: 'Topaz | Proof of existence in one line of code', // Navigation and Site Title
   siteTitleAlt: 'Topaz', // Alternative Site title for SEO
   siteUrl: 'https://topaz.io', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
