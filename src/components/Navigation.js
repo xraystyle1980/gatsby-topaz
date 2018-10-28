63f69c120a4b978439dab1735731aff2eb1f6d05
Matt Trice <matt.trice@gmail.com>
2018-10-28 15:07:29 -0400

    Navigation back

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,27 +5,18 @@ import IconMedium from '../assets/images/icon-medium.svg';
 import IconTwitter from '../assets/images/icon-twitter.svg';
 
 const Navigation = () => (
-  <header className="header__navigation">
-    <div className="grid-wrapper">
-      <div className="col-6 logo">
-        <a href="#home"><img src={logo} alt="Topaz" /></a>
-      </div>
-      <div className="col-6">
-        <nav className="navigation__desktop--white" role="navigation" aria-expanded="false" aria-label="Main menu">
-          <ul>
-            <li><a className="navlinks__desktop--white" href="#">About Topaz</a></li>
-            <li>
-              <ul className="icons">
-                <li><a href="#" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
-                <li><a href="#" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
+  <nav className="navigation__desktop--white" role="navigation" aria-expanded="false" aria-label="Main menu">
+    <ul>
+      <li><a className="navlinks__desktop--white" href="/about">About Topaz</a></li>
+      <li>
+        <ul className="icons">
+          <li><a href="#" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
+          <li><a href="#" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
+          <li><a href="#" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navigation;
