0482345c643f10a8ee1a75eebe60e0056d2129c9
Matt Trice <matt.trice@gmail.com>
2018-10-28 15:06:29 -0400

    About page in place

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,9 @@ import React from "react";
 import icon32 from '../assets/images/favicon-32x32.png';
 import Helmet from "react-helmet";
 import Mc from '../components/Mc';
+import SEO from '../components/SEO';
+
+import Header from "../components/Header";
 
 
 import Layout from '../components/layout';
@@ -13,86 +16,76 @@ import iconIntegrate from '../assets/images/icon-integrate.svg' ;
 import iconReporting from '../assets/images/icon-reporting.svg' ;
 import iconArrange from '../assets/images/icon-arrange.svg' ;
 import iconCheck from '../assets/images/icon-check.svg' ;
+import iconTrust from '../assets/images/icon-trust.svg' ;
+
+
+import iconCube from '../assets/images/icon-cube.svg' ;
+import iconCubeWhite from '../assets/images/icon-cube-white.svg' ;
+import AboutBlocks01 from '../assets/images/about-blocks-01.svg' ;
+import logoDecentGray from '../assets/images/logo-decent-gray.svg' ;
+import decentGroup from '../assets/images/decent-group.jpg' ;
 
 class Homepage extends React.Component {
     render() {
-        const siteTitle = "Topaz - Blockchain Simplified";
+        // const siteTitle = "Topaz - Blockchain Simplified";
 
         return (
             <Layout>
-              <Helmet
-                title="Topaz | Proof of existence in one line of code"
-                meta={[
-                  { name: 'description', content: '' },
-                  { name: 'keywords', content: '' },
-                  { name: 'robots', content: 'noindex, nofollow' },
-                ]}
-                link={[
-                  { rel: 'shortcut icon', sizes: '32x32', type: 'image/png', href: `${icon32}` }
-                ]}
-                />
 
-                  <section  className="features__hero--home">
-
-
+              <SEO />
+              <div className="header__background--gradient angle--bottom-right">
+                <div className="header__background--gradient-about-photo">
+                  <Header />
+                  <div className="header__background--content">
                     <div className="container">
                       <div className="grid-wrapper">
-                        <div className="col-12">
-                          <h2 className="features__headline--dark"><span>Add the value</span> of complex blockchain applications seamlessly to your existing technology stack.</h2>
+                        <div className="col-2"></div>
+                        <div className="col-8 align-center"><img src={iconCubeWhite} alt="Topaz" className="" />
+                          <h2 className="hero__headline--white">Bringin' the chain at enterprise scale.</h2>
                         </div>
+                        <div className="col-2"></div>
                       </div>
                     </div>
-
-                    <div className="container features__block">
-                      <div className="grid-wrapper">
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconEfficient} alt="Efficient" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">EFFICIENT</h4>
-                            <p className="features__paragraph--desc-dark">Store data on Ethereum in the most efficient way possible.</p>
-                          </div>
-                        </div>
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconIntegrate} alt="Integrate" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">INTEGRATE</h4>
-                            <p className="features__paragraph--desc-dark">Integrate with any existing codebase via first class packages.</p>
-                          </div>
-                        </div>
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconReporting} alt="Reporting" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">REPORTING</h4>
-                            <p className="features__paragraph--desc-dark">Generate reports on the data you store on blockchain.</p>
-                          </div>
-                        </div>
-                      </div>
+                  </div>
+                </div>
+              </div>
+              <section className="--content--padding">
+                <div className="container">
+                  <div className="grid-wrapper">
+                    <div className="col-6">
+                      <h2 class="hero__headline">How Topaz Works</h2>
+                      <p class="paragraph--larger">We are eliminating the massive cost of blockchain software (currently, accessible by hiring developers such as ourselves to produce custom software) by abstracting away the "blockchain". In other words, we are doing for blockchain what Amazon did for server-side software: making tools for developers to build things better, cheaper, and faster. We aim to make the hairy mess of blockchain go "poof" just like Amazon did with loud server racks. Our initial product is a data storage solution that allows users to send and retrieve data to a public blockchain (Ethereum) through simple API calls (Topaz:Blockchain :: Stripe:Payments :: Twilio:SMS).</p>
+                    </div>
+                    <div className="col-6 align-center">
+                      <img src={AboutBlocks01} alt="Topaz" className="about__image--blocks01" />
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section className="">
+                <div className="container">
+                  <div className="grid-wrapper">
+                    <div className="col-12">
+                      <h6 class="content__label--dark"><img src={iconCube} alt="Topaz" className="features__image--icon-small" />The Team</h6>
+                      <p class="paragraph--larger">We are eliminating the massive cost of blockchain software (currently, accessible by hiring developers such as ourselves to produce custom software) by abstracting away the "blockchain". In other words, we are doing for blockchain what Amazon did for server-side software: making tools for developers to build things better, cheaper, and faster. We aim to make the hairy mess of blockchain go "poof" just like Amazon did with loud server racks. Our initial product is a data storage solution that allows users to send and retrieve data to a public blockchain (Ethereum) through simple API calls (Topaz:Blockchain :: Stripe:Payments :: Twilio:SMS).</p>
+                      <p class="paragraph--larger blurb__decent"><img src={logoDecentGray} alt="decent" className="logo__decent" /><strong>decent</strong> is a software development and consultancy firm specializing in blockchain technology. Founded by expert software developers with extensive experience deploying production applications in dozens of programming languages-we are constructing the future of the digital economy, through cryptography.</p>
                     </div>
 
-                    <div className="container--blue">
-                      <div className="grid-wrapper">
-                        <div className="col-12">
-                          <div className="features__colorblock">
-                              <p className="features__paragraph--light"><span><img src={iconArrange} alt="Arrange" className="features__image--icon-small" />Topaz arranges your data</span> and performs Ethereum transactions on a set interval to store the data on a blockchain at no cost to you.</p>
-                              <img src={FeaturesBlocks01} alt="Topaz" className="features__image--blocks01" />
-                          </div>
-                        </div>
-                      </div>
+                  </div>
+                </div>
+              </section>
+              <section className="">
+                <div className="container">
+                  <div className="grid-wrapper">
+
+                    <div className="col-12">
+                      <h2 class="hero__headline">The Future of Finance</h2>
+                      <p class="paragraph--larger">By design, a blockchain is resistant to modification of the data. It is "an open, distributed ledger that can record transactions between two parties efficiently and in a verifiable and permanent way".[7] For use as a distributed ledger, a blockchain is typically managed by a peer-to-peer network collectively adhering to a protocol for inter-node communication and validating new blocks. Once recorded, the data in any given block cannot be altered retroactively without alteration of all subsequent blocks, which requires consensus of the network majority. Although blockchain records are not unalterable, blockchains may be considered secure by design and exemplify a distributed computing system with high Byzantine fault tolerance. Decentralized consensus has therefore been claimed with a blockchain.[8]</p>
                     </div>
 
-                    <div className="container">
-                      <div className="grid-wrapper">
-                        <div className="col-12">
-                          <div className="features__block--under">
-                          <img src={FeaturesBlocks02} alt="Topaz" className="features__image--blocks02" />
-                            <p className="features__paragraph--dark"><img src={iconCheck} alt="Success" className="features__image--icon-small" />You can now query and furnish your data, and provide <span>proof of existence and chain of custody</span> over time.</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                  </section>
-
+                  </div>
+                </div>
+              </section>
             </Layout>
         );
     }
