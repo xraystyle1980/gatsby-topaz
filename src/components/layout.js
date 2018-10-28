34bca193f014573281a23cd6922d6fc56a69ea5d
Matt Trice <matt.trice@gmail.com>
2018-10-28 15:01:54 -0400

    Add footer, index, and layout back

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,7 @@ import React from "react";
 import "../assets/scss/main.scss";
 import "../assets/scss/topaz.scss";
 
-import Navigation from "./Navigation";
-import Header from "./Header";
+
 import Footer from "./Footer";
 
 class Template extends React.Component {
@@ -30,18 +29,21 @@ class Template extends React.Component {
         const { children } = this.props;
 
         return (
-            <div className={`body ${this.state.loading}`}>
-              <div className="header__background--gradient angle--bottom-right">
-                <div className="header__background--logo">
-                  <Navigation />
-                  <Header />
-                </div>
-              </div>
-              {children}
-              <div className="header__background--gradient angle--top-left">
-                <Footer />
-              </div>
-            </div>
+          <div className={`body ${this.state.loading}`}>
+
+
+
+
+
+
+
+            {children}
+
+
+
+            <Footer />
+
+          </div>
         );
     }
 }
