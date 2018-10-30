89dde9ac7528c01be01e34a464df46e9be741708
Matt Trice <matt.trice@gmail.com>
2018-10-30 11:36:16 -0400

    Update social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,9 @@ class Footer extends React.Component {
                     <h2 className="align-center white signup__label">Sign up for updates from Topaz.</h2>
                     <Mc />
                     <ul className="footer__socialicons icons">
-                      <li><a href="#" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
-                      <li><a href="#" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
-                      <li><a href="#" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
+                    <li><a href="https://medium.com/topaztellsthetruth" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
+                    <li><a href="https://twitter.com/topaz_is_LIT" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
+                    <li><a href="https://www.instagram.com/topaztellsthetruth/" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
                     </ul>
                     <p className="copyright">&copy; Topaz, 2018</p>
                   </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,9 +7,9 @@ import IconTwitter from '../assets/images/icon-twitter.svg';
 const Navigation = () => (
   <nav className="navigation__desktop--white" role="navigation" aria-expanded="false" aria-label="Main menu">
     <ul className="icons">
-      <li><a href="#" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
-      <li><a href="#" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
-      <li><a href="#" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
+      <li><a href="https://medium.com/topaztellsthetruth" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
+      <li><a href="https://twitter.com/topaz_is_LIT" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
+      <li><a href="https://www.instagram.com/topaztellsthetruth/" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
     </ul>
   </nav>
 );
