238665a4866fbd5789e711159d0a626d046ff0d3
Matt Trice <matt.trice@gmail.com>
2018-10-29 22:06:09 -0400

    Work on mobile menu button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,10 +14,10 @@ const Navigation = () => (
         <input type="checkbox" id="nav-toggle" />
         <label for="nav-toggle"><span className="toggle-words">Menu</span></label>
         <div className="menu">
-          <ul>
+          <ul className="ul-menu__outer">
             <li><a className="navlinks__desktop--white" href="/about">About Topaz</a></li>
             <li>
-              <ul className="icons">
+              <ul className="ul-menu__inner-socialicons">
                 <li><a href="#" className="icon alt fa-medium white"><span className="label">Medium</span></a></li>
                 <li><a href="#" className="icon alt fa-twitter white"><span className="label">Twitter</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram white"><span className="label">Instagram</span></a></li>
