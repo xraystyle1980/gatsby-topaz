4edbd2bfcd8416949df137651e1aee92113056bd
Matt Trice <matt.trice@gmail.com>
2018-10-30 12:01:57 -0400

    Copy updated on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ class Homepage extends React.Component {
                         <div className="grid-wrapper">
                           <div className="col-12">
                             <h1 className="hero__headline--white">Topaz tells the truth.</h1>
-                            <p className="hero__paragraph--white"><strong>Welcome to the future of commerce,</strong> where transparency is demanded amongst businesses, consumers, and regulators. Powered by blockchain technology, Topaz enforces data integrity at enterprise scale.</p>
+                            <p className="hero__paragraph--white"><strong>Welcome to the future of commerce,</strong> where transparency is required by businesses, consumers, and regulators. Powered by blockchain technology, Topaz offers unparalleled data integrity at enterprise scale.</p>
                             <div className="col-12">
                               <h2 className="align-center white signup__label">Our developer sandbox is open. Come play.</h2>
                             </div>
@@ -47,65 +47,67 @@ class Homepage extends React.Component {
                   </div>
                 </div>
               </div>
-                  <section  className="features__hero--home">
-                    <div className="container">
-                      <div className="grid-wrapper">
-                        <div className="col-12">
-                          <h2 className="features__headline--dark">Integrate with any application without worrying about complex blockchain concepts - no transaction fees, no smart contract development, no cryptocurrencies - <span>just one line of code.</span></h2>
-                        </div>
+              <section  className="features__hero--home">
+                <div className="container features__block">
+                  <div className="grid-wrapper">
+                    <div className="col-4">
+                      <div className="features__block--shadow">
+                        <img src={iconTrust} alt="Efficient" className="features__image--icon" />
+                        <h4 className="features__header--desc-dark">TRUST</h4>
+                        <p className="features__paragraph--desc-dark">Embed trust in any data and secure it with any blockchain.</p>
                       </div>
                     </div>
-
-                    <div className="container features__block">
-                      <div className="grid-wrapper">
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconTrust} alt="Efficient" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">TRUST</h4>
-                            <p className="features__paragraph--desc-dark">Process data at enterprise scale for referencing and securing in any blockchain.</p>
-                          </div>
-                        </div>
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconIntegrate} alt="Integrate" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">VERIFY</h4>
-                            <p className="features__paragraph--desc-dark">Prove the integrity of data and provide chain of custody over time.</p>
-                          </div>
-                        </div>
-                        <div className="col-4">
-                          <div className="features__block--shadow">
-                            <img src={iconReporting} alt="Reporting" className="features__image--icon" />
-                            <h4 className="features__header--desc-dark">REPORT</h4>
-                            <p className="features__paragraph--desc-dark">Generate reports to give auditors and customers complete peace of mind.</p>
-                          </div>
-                        </div>
+                    <div className="col-4">
+                      <div className="features__block--shadow">
+                        <img src={iconIntegrate} alt="Integrate" className="features__image--icon" />
+                        <h4 className="features__header--desc-dark">VERIFY</h4>
+                        <p className="features__paragraph--desc-dark">Prove the integrity of data and provide accurate chain of custody of assets over time.</p>
                       </div>
                     </div>
-
-                    <div className="container--blue">
-                      <div className="grid-wrapper">
-                        <div className="col-12">
-                          <div className="features__colorblock">
-                              <p className="features__paragraph--light"><span><img src={iconArrange} alt="Arrange" className="features__image--icon-small" />Topaz runs alongside any existing application,</span> extracting data and processing it to be referenced and secured by any blockchain - at enterprise scale.</p>
-                              <img src={FeaturesBlocks01} alt="Topaz" className="features__image--blocks01" />
-                          </div>
-                        </div>
+                    <div className="col-4">
+                      <div className="features__block--shadow">
+                        <img src={iconReporting} alt="Reporting" className="features__image--icon" />
+                        <h4 className="features__header--desc-dark">REPORT</h4>
+                        <p className="features__paragraph--desc-dark">Generate reports to give auditors, customers, and partners complete peace of mind.</p>
                       </div>
                     </div>
+                  </div>
+                </div>
 
-                    <div className="container">
-                      <div className="grid-wrapper">
-                        <div className="col-12">
-                          <div className="features__block--under">
-                          <img src={FeaturesBlocks02} alt="Topaz" className="features__image--blocks02" />
-                            <p className="features__paragraph--dark"><img src={iconCheck} alt="Success" className="features__image--icon-small" />For the first time in history, accountability can be applied to any piece of software - at scale. Help <span>revolutionize the way business</span> is conducted from the ground up.</p>
-                          </div>
-                        </div>
+                <div className="container">
+                  <div className="grid-wrapper">
+                    <div className="col-12">
+                      <h2 className="features__headline--dark">Integrate with any application without worrying about the complexities of decentralized systems - no transaction fees, no smart contract development, no cryptocurrencies - <span>just one line of code.</span></h2>
+
+
+                    </div>
+                  </div>
+                </div>
+
+                <div className="container--blue">
+                  <div className="grid-wrapper">
+                    <div className="col-12">
+                      <div className="features__colorblock">
+                          <p className="features__paragraph--light"><img src={iconArrange} alt="Arrange" className="features__image--icon-small" />Topaz runs alongside <span>any existing software,</span> extracting data and processing it to be trusted and secured.</p>
+
+
+                          <img src={FeaturesBlocks01} alt="Topaz" className="features__image--blocks01" />
                       </div>
                     </div>
+                  </div>
+                </div>
 
-                  </section>
-
+                <div className="container">
+                  <div className="grid-wrapper">
+                    <div className="col-12">
+                      <div className="features__block--under">
+                      <img src={FeaturesBlocks02} alt="Topaz" className="features__image--blocks02" />
+                        <p className="features__paragraph--dark"><img src={iconCheck} alt="Success" className="features__image--icon-small" />Technology has evolved to enforce transparency throughout the entire economy. Help <span>revolutionize the way business is conducted</span> from the inside out.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </Layout>
         );
     }
